771f66d0c753d5cb07ce46c2dbf321000fade03f
fariha <fariha.fariha.chy@gmail.com>
2020-06-25 20:00:26 +0600

    publishing website
    happy

---
+++ portfolio 2/script.js
@@ -0,0 +1,52 @@
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+const textSpan = document.querySelector(".txt-type");
+
+
+const textArray=["WELCOME TO MY WEBSITE" ,"I AM ABDULLAH MOOSA SYED" , "ELECTRICAL ENGINEER" ];
+const typingDelay=130;
+const erasingDelay=100;
+const nextTextDelay=1500;
+let textArrayIndex=0;
+let charIndex=0;
+
+   function type(){
+       if(charIndex < textArray[textArrayIndex].length){
+           textSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+           charIndex++;
+           setTimeout(type , typingDelay);
+       }
+       else{
+        setTimeout(erase , nextTextDelay);
+       }
+   }
+
+   function erase(){
+       if( charIndex > 0){
+        textSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+       }
+       else{
+           textArrayIndex++;
+           if(textArrayIndex >= textArray.length) textArrayIndex=0;
+           setTimeout(type, typingDelay + 1100);
+       }
+   }
+
+ 
+   document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(type , nextTextDelay + 100);
+   });
+
+   AOS.init({
+    easing:'ease',
+    duration:1800
+});
